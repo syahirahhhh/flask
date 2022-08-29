4ba2d1ac1ff357b02bb35b7189686647975ea7bc
syahirahhhh <109711768+syahirahhhh@users.noreply.github.com>
2022-08-29 12:18:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+import logging
+
 app = Flask(__name__)
+logging.basicConfig(filename='records.log', level=logging.DEBUG, format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
 
 @app.route('/')
-def hello_world():
-    return 'Hello, Docker!'
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+def home():
+   app.logger.info('Info level log')
+   app.logger.warning('Warning level log')
+   return render_template('index.html')
+   
+if __name__ == '__main__':
+   app.run()
